57f7fb7cc52f3ca7e6019518e6727efeaa4b3d4c
leoliao01 <144210423+leoliao01@users.noreply.github.com>
2023-09-06 15:58:49 +0800

    Add files via upload

---
+++ week1/temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+h=float(input("請輸入您的身高(公尺):"))
+w=float(input("請輸入您的體重(公斤):"))
+
+BMI=w/(h**2)
+print(f"您的BMI是:{BMI:.2f}")
